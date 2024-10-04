3383420377e13500165b1127d2c51908a2a5cc54
Lisii <lisiyao20041017@outlook.com>
2024-10-04 13:23:56 +0800

    simplify main func

--- re-uv_ws/src/uv-vision/src/main.cpp
+++ re-uv_ws/src/uv-vision/src/main.cpp
@@ -1,12 +1,16 @@
 #include "communicator.hpp"
 #include "visual_tracker.hpp"
 
-int main(int argc, char **argv) {
-  reuv::VisualTracker visual_tracker(
-      "/workspaces/reuv/re-uv_ws/src/uv-vision/src/test.mp4");
-  // reuv::Communicator communicator(visual_tracker);
-  visual_tracker.track();
-  // communicator.start();
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    auto communicator = std::make_shared<reuv::Communicator>(
+        "communicator",
+        reuv::VisualTracker("/workspaces/reuv/re-uv_ws/src/uv-vision/src/test.mp4"),
+        "/dev/ttyUSB0",
+        100);
+    rclcpp::spin(communicator);
+    rclcpp::shutdown();
 
-  return 0;
+    return 0;
 }
